9c777f8d5629d5b01daf482b6180a4a6816909f8
Denesh Shan <denesh.shan@outlook.com>
2015-02-19 15:10:07 +0000

    Committing chapters 4 and 5

--- 2nd_ed/4/geometry.erl
+++ 2nd_ed/4/geometry.erl
@@ -1,7 +1,15 @@
 -module(geometry).
--export([area/1]).
+-export([test/0,area/1]).
+
+test() ->
+  12            = area({rectangle, 3, 4}),
+  144           = area({square, 12}),
+  6.28318530718 = area({circle, 2}),
+  3.0           = area({triangle, 2, 3}),
+  tests_worked.
+
 
 area({rectangle, Width, Height})         -> Width * Height;
 area({square, Side})                     -> Side * Side;
 area({circle, Radius})                   -> 3.14159265359 * Radius;
-area({triangle_r_angled, Width, Height}) -> Width * Height / 2.
+area({triangle, Width, Height}) -> Width * Height / 2.
